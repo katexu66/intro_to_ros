06b5dcc1d65a8927c31b7aae9fc6a2d61e2ad10d
katexu66 <katexu66@gmail.com>
2024-07-17 14:50:45 -0400

    Bluerov2_sensors problem 1 working

--- intro_to_ros/bluerov2_sensors.py
+++ intro_to_ros/bluerov2_sensors.py
@@ -4,37 +4,48 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import BatteryState
 from sensor_msgs.msg import Imu
+from rclpy.qos import QoSProfile, QoSHistoryPolicy, QoSReliabilityPolicy, QoSDurabilityPolicy
 
-class SensorSubscriber(Node):
-    
-    battery_message = BatteryState()
-    IMU_message = Imu()
+# computer IP: 169.254.68.200
+
+class BlueRov2Sensors(Node):
     
     def __init__(self):
-        super().__init__("Battery sensor messages")
+        super().__init__("bluerov2_sensors")
         
+        qos_profile = QoSProfile(
+                history=QoSHistoryPolicy.KEEP_LAST,
+                depth=10,
+                reliability=QoSReliabilityPolicy.BEST_EFFORT,
+                durability=QoSDurabilityPolicy.VOLATILE
+            )
+        
+        self.battery_message = None
         self.battery_subscriber = self.create_subscription(
             BatteryState, # message type
             "/mavros/battery", # topic
-            self.callback,
-            10
+            self.battery_callback,
+            qos_profile
             )
         
-        # self.IMU_subscriber = self.create_subscription(
-        #     Imu, # message type
-        #     "/mavros/imu/data", # topic
-        #     self.callback,
-        #     10
-        #     )
+        self.IMU_message = None
+        self.IMU_subscriber = self.create_subscription(
+            Imu, # message type
+            "/mavros/imu/data", # topic
+            self.IMU_callback,
+            qos_profile
+            )
         
-        self.battery_subscriber
-        # self.IMU_subscriber
+        self.battery_param = BatteryState()
+        
         self.get_logger().info("starting subscriber node")
         
-    def callback(self, battery_message): # for each subscriber need callback method
-        # battery_message = BatteryState()
-        # IMU_message = Imu()
-        self.get_logger().info(f"Battery state: {battery_message}")
+    def battery_callback(self, msg): # for each subscriber need callback method
+        self.battery_param = msg
+        self.get_logger().info(f"Voltage: {msg.voltage}, current: {msg.current}")
+        
+    def IMU_callback(self, msg):
+        pass
         
 def main(args=None):
     rclpy.init(args=args)
